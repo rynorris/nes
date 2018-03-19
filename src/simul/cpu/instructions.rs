583080339b1dd7f3ff60f3a424ff3c6d3e3c00f8
DiscoViking <rynorris@gmail.com>
2018-03-19 23:58:29 +0900

    Implementation of ADC

--- src/simul/cpu/instructions.rs
+++ src/simul/cpu/instructions.rs
@@ -1,4 +1,5 @@
 use simul::cpu;
+use simul::utils;
 
 pub type Operation = fn(&mut cpu::CPU, cpu::addressing::AddressingMode) -> u32;
 
@@ -19,6 +20,7 @@ fn update_negative_flag(cpu: &mut cpu::CPU, result: u8) {
 }
 
 // LDA: Load Accumulator with Memory
+// A -> M
 pub fn lda(cpu: &mut cpu::CPU, load_addr: cpu::addressing::AddressingMode) -> u32 {
     let (addr, addr_cycles) = load_addr(cpu);
     let res = cpu.load_memory(addr);
@@ -30,9 +32,55 @@ pub fn lda(cpu: &mut cpu::CPU, load_addr: cpu::addressing::AddressingMode) -> u3
 }
 
 // STA: Store Accumulator in Memory
+// M -> A
 pub fn sta(cpu: &mut cpu::CPU, load_addr: cpu::addressing::AddressingMode) -> u32 {
     let (addr, addr_cycles) = load_addr(cpu);
     let byte = cpu.a;
     cpu.store_memory(addr, byte);
+    addr_cycles
+}
+
+// ADC: Add Memory to Accumulator with Carry
+// A + M + C -> A
+pub fn adc(cpu: &mut cpu::CPU, load_addr: cpu::addressing::AddressingMode) -> u32 {
+    let (addr, addr_cycles) = load_addr(cpu);
+    let mem = cpu.load_memory(addr);
+
+    let carry_val: u8 = if cpu.p.is_set(cpu::flags::Flag::C) { 1 } else { 0 };
+    let (res, carry) = if cpu.p.is_set(cpu::flags::Flag::D) {
+        // BCD arithmetic.
+        let hex_a = utils::bcd_to_hex(cpu.a);
+        let hex_mem = utils::bcd_to_hex(mem);
+
+        // Cannot be > 255.
+        let hex_res = hex_a + hex_mem + carry_val;
+        (utils::hex_to_bcd(hex_res), hex_res > 99)
+    } else {
+        // Normal arithmetic.
+        let (res, carry1) = cpu.a.overflowing_add(mem);
+        let (res, carry2) = res.overflowing_add(carry_val);
+        (res, carry1 || carry2)
+    };
+    
+    // Set carry flag.
+    if carry {
+        cpu.p.set(cpu::flags::Flag::C);
+    } else {
+        cpu.p.clear(cpu::flags::Flag::C);
+    }
+
+    // Set overflow flag.
+    let old_sign = cpu.a | 0b1000_0000;
+    let new_sign = res | 0b1000_0000;
+    if new_sign != old_sign {
+        cpu.p.set(cpu::flags::Flag::V);
+    } else {
+        cpu.p.clear(cpu::flags::Flag::V);
+    }
+
+    update_zero_flag(cpu, res);
+    update_negative_flag(cpu, res);
+
+    cpu.a = res;
     addr_cycles
 }
--- src/simul/util.rs
+++ src/simul/util.rs
@@ -2,7 +2,51 @@ pub fn combine_bytes(high: u8, low: u8) -> u16 {
     ((high as u16) << 8) + (low as u16)
 }
 
+pub fn bcd_to_hex(bcd: u8) -> u8 {
+    let high_nibble = bcd >> 4;
+    let low_nibble = bcd & 0x0F;
+    (high_nibble * 10) + low_nibble
+}
+
+pub fn hex_to_bcd(hex: u8) -> u8 {
+    if hex > 99 {
+        panic!("Integers greater than 99 cannot be represented in a BCD byte.");
+    }
+
+    let tens = hex / 10;
+    let units = hex % 10;
+    (tens << 4) | units
+}
+
 #[test]
 fn test_combine_bytes() {
     assert_eq!(combine_bytes(0x12, 0xAB), 0x12AB);
 }
+
+macro_rules! b2h_test {
+    ($name:ident: $bcd:expr => $hex:expr) => {
+        #[test]
+        fn $name() {
+            assert_eq!(bcd_to_hex($bcd), $hex);
+        }
+    };
+}
+
+b2h_test!(test_b2h_0: 0b0000_0000 => 0);
+b2h_test!(test_b2h_9: 0b0000_1001 => 9);
+b2h_test!(test_b2h_11: 0b0001_0001 => 11);
+b2h_test!(test_b2h_98: 0b1001_1000 => 98);
+
+macro_rules! h2b_test {
+    ($name:ident: $hex:expr => $bcd:expr) => {
+        #[test]
+        fn $name() {
+            assert_eq!(hex_to_bcd($hex), $bcd);
+        }
+    };
+}
+
+h2b_test!(test_h2b_0: 0 => 0b0000_0000);
+h2b_test!(test_h2b_9: 9 => 0b0000_1001);
+h2b_test!(test_h2b_11: 11 => 0b0001_0001);
+h2b_test!(test_h2b_98: 98 => 0b1001_1000);
