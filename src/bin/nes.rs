80aee2f12bae931c453f79a5901a1ed4b00f27e3
DiscoViking <rynorris@gmail.com>
2018-11-03 21:05:49 +0900

    Remove unused code from nes.rs

--- src/bin/nes.rs
+++ src/bin/nes.rs
@@ -1,9 +1,6 @@
 extern crate mos_6500;
 
 use std::env;
-use std::fs::File;
-use std::io::Write;
-use std::path::Path;
 
 use mos_6500::emulator;
 use mos_6500::emulator::ines;
@@ -14,17 +11,6 @@ fn main() {
     let rom_path = match args.get(2) {
         None => panic!("You must pass in a path to a iNes ROM file."),
         Some(path) => path,
-    };
-
-    let trace_enabled = match env::var("NES_TRACE") {
-        Err(_) => false,
-        Ok(val) => val == "1",
-    };
-
-    let trace_out_path = Path::new("./cpu.trace");
-    let trace_out = match File::create(trace_out_path) {
-        Err(cause) => panic!("Couldn't open {}: {}", trace_out_path.display(), cause),
-        Ok(file) => file,
     };
 
     let rom = ines::ROM::load(rom_path);
