68b8114f2d5a514f4d79d7daf06f27e78483bcad
DiscoViking <rynorris@gmail.com>
2018-11-07 23:01:47 +0900

    Remove code which wrote audio to files

--- src/ui/audio.rs
+++ src/ui/audio.rs
@@ -1,5 +1,4 @@
-use std::io::{BufWriter, Write};
-use std::fs::File;
+use std::io::Write;
 use std::cell::RefCell;
 use std::rc::Rc;
 
@@ -13,7 +12,6 @@ pub const SAMPLE_RATE: f32 = 48_000.0;
 pub struct AudioQueue {
     output: Rc<RefCell<SimpleAudioOut>>,
     queue: audio::AudioQueue<f32>,
-    file: BufWriter<File>,
 }
 
 impl AudioQueue {
@@ -30,36 +28,20 @@ impl AudioQueue {
             Ok(q) => q,
         };
 
-        let file = match File::create("audio.wav") {
-            Err(cause) => panic!("Failed to open audio file: {}", cause),
-            Ok(f) => f,
-        };
-
         queue.resume();
 
         AudioQueue {
             output,
             queue,
-            file: BufWriter::new(file),
         }
     }
 
     pub fn flush(&mut self) {
         let mut output = self.output.borrow_mut();
         let queue = &mut self.queue;
-        let file = &mut self.file;
         let request_samples = SAMPLE_RATE / (RENDER_FPS as f32);
         output.consume(request_samples as usize, |data| {
             queue.queue(&data);
-            let mut frame_file = match File::create("audio_frame.wav") {
-                Err(cause) => panic!("Failed to open audio file: {}", cause),
-                Ok(f) => f,
-            };
-            for ix in 0 .. data.len() {
-                let pcm_sample = (data[ix] * 128.0 + 128.0) as u8;
-                file.write(&[pcm_sample]).unwrap();
-                frame_file.write(&[pcm_sample]).unwrap();
-            }
         });
     }
 
