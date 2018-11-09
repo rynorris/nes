6111a7292f81c5bcbb4cbca1ae0491a3198e8aed
DiscoViking <rynorris@gmail.com>
2018-11-10 01:33:13 +0900

    Add CNROM

--- src/emulator/mappers/mod.rs
+++ src/emulator/mappers/mod.rs
@@ -1,5 +1,13 @@
-mod nrom;
-mod mmc1;
+// In iNES mapper number order.
 
+// #1 NROM
+mod nrom;
 pub use self::nrom::NROM;
+
+// #3 CNROM
+mod cnrom;
+pub use self::cnrom::CNROM;
+
+// #4 MMC1
+mod mmc1;
 pub use self::mmc1::MMC1;
