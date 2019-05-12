f22317a04ac662b779d72a1b2f8d1d593407b89e
Ryan Norris <rynorris@gmail.com>
2019-05-12 23:34:17 +0900

    Compile to wasm (#63)
    * Checkpoint
    
    * Fix compile
    
    * Fix compile
    
    * Fix wasm build
    
    * WASM Events API
    
    * Example app
    
    * fmt
    
    * Remove unnecessary stuff

---
+++ nes_web/example/index.js
@@ -0,0 +1,144 @@
+import { Emulator, Event, Key } from "nes_web";
+
+const nes_audio = new AudioContext();
+
+const render = nes => {
+    const screen = document.getElementById("screen");
+    const tgt = document.createElement("canvas");
+    tgt.width = 256;
+    tgt.height = 240;
+
+    const ctx = tgt.getContext("2d");
+    const img = ctx.createImageData(256, 240);
+    const frame = nes.get_frame();
+    for (let i = 0; i < img.data.length / 4; i++) {
+        // RGBA
+        img.data[i * 4] = frame[i * 3];
+        img.data[i * 4 + 1] = frame[i * 3 + 1];
+        img.data[i * 4 + 2] = frame[i * 3 + 2];
+        img.data[i * 4 + 3] = 255;
+    }
+    ctx.putImageData(img, 0, 0);
+
+    screen.getContext("2d").drawImage(tgt, 0, 0, 512, 480);
+};
+
+var BUF_LENGTH = 0.1;
+var nextStartTime = 0;
+
+const queue_audio = (nes, cycles, samples) => {
+    const buf = nes.get_audio(BigInt(cycles), BigInt(samples))
+    if (buf.length === 0) {
+        return;
+    }
+
+    const ctx = nes_audio;
+    const audioBuffer = ctx.createBuffer(1, buf.length, 48000);
+    audioBuffer.getChannelData(0).set(buf);
+    const source = ctx.createBufferSource();
+    source.buffer = audioBuffer;
+    source.connect(ctx.destination);
+
+    const bufLength = audioBuffer.length / audioBuffer.sampleRate;
+
+    if (nextStartTime === 0) {
+        // Initial buffer time.
+        nextStartTime = ctx.currentTime + BUF_LENGTH;
+    }
+
+    source.start(nextStartTime);
+
+    nextStartTime += bufLength;
+
+    // Clamp audio latency.
+    if (nextStartTime < ctx.currentTime || nextStartTime > ctx.currentTime + BUF_LENGTH) {
+        // Reset buffer.
+        nextStartTime = ctx.currentTime + BUF_LENGTH;
+        console.log("Reset audio latency");
+    }
+}
+
+var stopRunning = () => {};
+
+const selectRom = files => {
+    if (files.length !== 1) {
+        throw new Error("Must select exactly 1 file");
+    }
+
+    stopRunning();
+
+    const file = files[0];
+    const reader = new FileReader();
+    var nes = null;
+    var running = true;
+
+    reader.onloadend = () => {
+        const array = reader.result;
+        nes = Emulator.new(new Uint8Array(array));
+
+        var ts = null;
+
+        function render_cb(timestamp) {
+            var cycles = nes.run(100000);
+            render(nes);
+
+            if (ts !== null) {
+                const dt = timestamp - ts;
+                queue_audio(nes, cycles, Math.round(48000 / 60));
+            }
+
+            if (running) {
+                window.requestAnimationFrame(render_cb);
+            }
+
+            ts = timestamp;
+            cycles = BigInt(0);
+        }
+
+        stopRunning = () => running = false;
+
+        nes_audio.resume();
+        window.requestAnimationFrame(render_cb);
+    }
+
+    reader.readAsArrayBuffer(file);
+
+    document.onkeydown = event => {
+        var key = convertJsKeyCode(event.key);
+        if (key !== null) {
+            nes.broadcast(Event.key_down(key));
+        }
+    };
+
+    document.onkeyup = event => {
+        var key = convertJsKeyCode(event.key);
+        if (key !== null) {
+            nes.broadcast(Event.key_up(key));
+        }
+    };
+};
+
+const convertJsKeyCode = key => {
+    switch (key) {
+        case "a":
+            return Key.A;
+        case "s":
+            return Key.S;
+        case "x":
+            return Key.X;
+        case "z":
+            return Key.Z;
+        case "ArrowLeft":
+            return Key.Left;
+        case "ArrowRight":
+            return Key.Right;
+        case "ArrowUp":
+            return Key.Up;
+        case "ArrowDown":
+            return Key.Down;
+        default:
+            return null;
+    }
+};
+
+window.selectRom = selectRom;
---
+++ nes_web/src/lib.rs
@@ -0,0 +1,69 @@
+pub mod event;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use wasm_bindgen::prelude::*;
+
+use nes::emulator::ines;
+use nes::emulator::io;
+use nes::emulator::io::event::EventBus;
+use nes::emulator::NES;
+
+#[wasm_bindgen]
+pub struct Emulator {
+    nes: NES,
+    event_bus: Rc<RefCell<EventBus>>,
+    video_out: Rc<RefCell<io::Screen>>,
+    audio_out: Rc<RefCell<io::SimpleAudioOut>>,
+}
+
+#[wasm_bindgen]
+impl Emulator {
+    pub fn new(rom_data: Vec<u8>) -> Emulator {
+        let event_bus = Rc::new(RefCell::new(EventBus::new()));
+        let video_out = Rc::new(RefCell::new(io::Screen::new()));
+        let audio_out = Rc::new(RefCell::new(io::SimpleAudioOut::new(48_000.0)));
+        let rom = ines::ROM::from_bytes(rom_data);
+
+        let nes = NES::new(event_bus.clone(), video_out.clone(), audio_out.clone(), rom);
+
+        Emulator {
+            nes,
+            event_bus,
+            video_out,
+            audio_out,
+        }
+    }
+
+    pub fn run(&mut self, ticks: u32) -> u64 {
+        self.nes.tick_multi(ticks)
+    }
+
+    pub fn get_frame(&self) -> Vec<u8> {
+        let mut buf = [0; 256 * 240 * 3];
+        self.video_out.borrow().do_render(|frame| {
+            for (tgt, src) in buf.iter_mut().zip(frame.iter()) {
+                *tgt = *src;
+            }
+        });
+
+        return buf.to_vec();
+    }
+
+    pub fn get_audio(&self, master_cycles: u64, num_samples: u64) -> Vec<f32> {
+        let mut buf: Vec<f32> = vec![];
+        self.audio_out
+            .borrow_mut()
+            .consume(master_cycles, num_samples, |audio| {
+                buf.extend_from_slice(audio);
+            });
+        return buf;
+    }
+
+    pub fn broadcast(&self, e: event::Event) {
+        let internal_event = event::convert_wasm_event_to_internal(e);
+        println!("{:?}", internal_event);
+        self.event_bus.borrow_mut().broadcast(internal_event);
+    }
+}
