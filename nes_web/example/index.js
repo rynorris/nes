648b8eb80635ca2dae1879d2303f2de08fc332ff
Ryan Norris <rynorris@gmail.com>
2019-05-13 14:29:50 +0900

    Run wasm emulator in web worker (#64)
    * Run emulator in web worker
    
    * Reset to normal speed

--- nes_web/example/index.js
+++ nes_web/example/index.js
@@ -1,8 +1,7 @@
-import { Emulator, Event, Key } from "nes_web";
-
 const nes_audio = new AudioContext();
+const worker = new Worker("worker.js");
 
-const render = nes => {
+const render = frame => {
     const screen = document.getElementById("screen");
     const tgt = document.createElement("canvas");
     tgt.width = 256;
@@ -10,7 +9,6 @@ const render = nes => {
 
     const ctx = tgt.getContext("2d");
     const img = ctx.createImageData(256, 240);
-    const frame = nes.get_frame();
     for (let i = 0; i < img.data.length / 4; i++) {
         // RGBA
         img.data[i * 4] = frame[i * 3];
@@ -26,8 +24,7 @@ const render = nes => {
 var BUF_LENGTH = 0.1;
 var nextStartTime = 0;
 
-const queue_audio = (nes, cycles, samples) => {
-    const buf = nes.get_audio(BigInt(cycles), BigInt(samples))
+const queue_audio = buf => {
     if (buf.length === 0) {
         return;
     }
@@ -54,7 +51,6 @@ const queue_audio = (nes, cycles, samples) => {
     if (nextStartTime < ctx.currentTime || nextStartTime > ctx.currentTime + BUF_LENGTH) {
         // Reset buffer.
         nextStartTime = ctx.currentTime + BUF_LENGTH;
-        console.log("Reset audio latency");
     }
 }
 
@@ -74,71 +70,24 @@ const selectRom = files => {
 
     reader.onloadend = () => {
         const array = reader.result;
-        nes = Emulator.new(new Uint8Array(array));
-
-        var ts = null;
-
-        function render_cb(timestamp) {
-            var cycles = nes.run(100000);
-            render(nes);
-
-            if (ts !== null) {
-                const dt = timestamp - ts;
-                queue_audio(nes, cycles, Math.round(48000 / 60));
-            }
-
-            if (running) {
-                window.requestAnimationFrame(render_cb);
-            }
-
-            ts = timestamp;
-            cycles = BigInt(0);
-        }
-
-        stopRunning = () => running = false;
-
-        nes_audio.resume();
-        window.requestAnimationFrame(render_cb);
+        
+        worker.postMessage({ kind: "rom", data: array }, [array]);
     }
 
     reader.readAsArrayBuffer(file);
-
-    document.onkeydown = event => {
-        var key = convertJsKeyCode(event.key);
-        if (key !== null) {
-            nes.broadcast(Event.key_down(key));
-        }
-    };
-
-    document.onkeyup = event => {
-        var key = convertJsKeyCode(event.key);
-        if (key !== null) {
-            nes.broadcast(Event.key_up(key));
-        }
-    };
 };
 
-const convertJsKeyCode = key => {
-    switch (key) {
-        case "a":
-            return Key.A;
-        case "s":
-            return Key.S;
-        case "x":
-            return Key.X;
-        case "z":
-            return Key.Z;
-        case "ArrowLeft":
-            return Key.Left;
-        case "ArrowRight":
-            return Key.Right;
-        case "ArrowUp":
-            return Key.Up;
-        case "ArrowDown":
-            return Key.Down;
-        default:
-            return null;
-    }
+document.onkeydown = event => {
+    worker.postMessage({ kind: "keydown", key: event.key });
+};
+
+document.onkeyup = event => {
+    worker.postMessage({ kind: "keyup", key: event.key });
+};
+
+worker.onmessage = ({ data }) => {
+    queue_audio(new Float32Array(data.audio));
+    render(new Uint8Array(data.video));
 };
 
 window.selectRom = selectRom;
--- nes_web/example/webpack.config.js
+++ nes_web/example/webpack.config.js
@@ -1,14 +1,32 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
-module.exports = {
-  entry: "./bootstrap.js",
+const appConfig = {
+  entry: {
+      app: "./bootstrap.js",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
+    filename: "[name].js",
+    chunkFilename: "app.[name].js",
   },
   mode: "development",
   plugins: [
     new CopyWebpackPlugin(['index.html'])
   ],
 };
+
+const workerConfig = {
+  entry: {
+      worker: "./bootstrap_worker.js",
+  },
+  target: "webworker",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    chunkFilename: "worker.[name].js",
+  },
+  mode: "development",
+};
+
+module.exports = [appConfig, workerConfig];
